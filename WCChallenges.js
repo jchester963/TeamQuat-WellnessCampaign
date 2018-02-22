a0a3d39155f523acccf1efcd33ce4a54740e345b
mweary <33878302+mweary@users.noreply.github.com>
2018-02-22 15:48:26 -0700

    .js - added comments
    Added comments to organize eventual code and prioritize deliverables.

--- WCChallenges.js
+++ WCChallenges.js
@@ -1,4 +1,16 @@
 var main = function(){
+  //(1) Hunger Scale Challenge
 
-
+  //(2) Fruit and Veg Challenge
+  
+  //(3) No Added Sugar Challenge
+  
+  //(4) Clean Eating Challenge
+    //combination of fruit and veg + no added sugar + Altman
+  
+  //(5) Pedometer
+  
+  //(6*) Altman Rule Calculator / Barcode Scanner
+    //primarily research
+  
 }$(document).ready(main);
